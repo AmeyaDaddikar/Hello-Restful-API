36516d8dacd3536d9bb12228a8990ac2a8b55545
Ameya Daddikar <ameyadaddikar@gmail.com>
2018-12-29 17:22:27 +0530

    Added ajaxHandler with it's private callback helper function

--- src/ajaxHandler.js
+++ src/ajaxHandler.js
@@ -5,7 +5,41 @@
  * 
 */
 
-// @TODO create an handler with /hello route and 404 error route
-let ajaxHandler = {};
+
+// helper function: used by all paths to generate their HTTP/HTTPS response
+const callback = function (statusCode = 500, payload = {}, res) {
+
+  const responseString = JSON.stringify(payload);
+
+  res.setHeader('Content-Type', 'application/json');
+  res.writeHead(statusCode);
+  res.end(responseString);
+
+}
+
+let ajaxHandler = {
+  paths : {},
+};
+
+// /hello path handler
+ajaxHandler.paths.hello = (data, res) => {
+  callback(200, {response : 'Hello'}, res);
+}
+
+// 404 handler
+ajaxHandler.NOT_FOUND = (data, res) => {
+  callback(404, {}, res);
+};
+
+ajaxHandler.handle = function (data, res) {
+
+  // select handler function as per path
+  let pathHandler = this.paths[data.path];
+
+  if (typeof(pathHandler) === 'undefined')
+    pathHandler = this.NOT_FOUND;
+  
+  pathHandler(data, res);
+};
 
 module.exports = ajaxHandler;
